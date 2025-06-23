861ac17917c1ebfd4d779c6d2835a7ec84bf111f
Samuel Emeka <devsamuel0611@gmail.com>
2025-06-23 16:50:58 +0100

    auth module added

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    RouterModule,
+    FormsModule,
+  ],
+})
+export class AuthModule {}

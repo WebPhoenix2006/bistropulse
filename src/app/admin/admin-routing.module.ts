cd180db173a94ecccd25314ec1aabc58736e54df
Samuel Emeka <devsamuel0611@gmail.com>
2025-06-28 11:59:00 +0100

    updated sidebar

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
       {
         path: 'dashboard',
         component: DashboardComponent,
-        // canActivate: [authGuard],
+        canActivate: [authGuard],
       },
       {
         path: 'restaurant-list',

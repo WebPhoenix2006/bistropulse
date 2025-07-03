022f21ff4d6b5ba60e209724f57dcf79f64b6304
Viccodes <ayeniv69@gmail.com>
2025-07-03 16:20:16 +0100

    trying to fix the popup button

---
+++ src/app/shared/components/popup/popup.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popup',
+  standalone: false,
+  templateUrl: './popup.component.html',
+  styleUrl: './popup.component.scss',
+})
+export class PopupComponent {}

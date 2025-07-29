49a341825fdd3f8be0c38fea63847b6fed8beb14
Viccodes <ayeniv69@gmail.com>
2025-07-29 10:17:48 +0100

    fixed the large loading animation

--- src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './loading-spinner.component.html',
   styleUrl: './loading-spinner.component.scss',
 })
-export class LoadingSpinnerComponent {}
+export class LoadingSpinnerComponent {
+  @Input() loadingText?: string;
+}
